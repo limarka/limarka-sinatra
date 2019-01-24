2e5fb1ebeeb12998f007de4411c52b17bdf148a7
cauachagas <cauatunas@gmail.com>
2019-01-24 20:24:25 -0300

    Colocando numa única página

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
+#!/usr/bin/env ruby
 # encoding: utf-8
 require 'sinatra'
+#require "sinatra/config_file"
 require 'json'
 require 'yaml/store'
-
+require 'yaml'
 
 
 #set :port, 8080
@@ -27,19 +29,6 @@ ListaTab = {
 }
 
 
-def store_name(filename, string)
-  File.open(filename, "a+") do |file|
-    file.puts(string+"\n")
-  end
-end
-
-def store_yaml(filename, string)
-  File.open(filename, "a+") do |file|
-    #file.puts("---\n"+string+"\n\n---")
-    file.puts(string)
-  end
-end
-
 def hash_to_yaml(h)
   s = StringIO.new
   s << h.to_yaml
@@ -59,7 +48,7 @@ def lista_siglas(sigla)
       h[campo] = sa.empty? ? nil : sa
     end
   end
-  return h
+  return h['siglas']
 end
 
 def lista_simbolos(simbolo)
@@ -75,44 +64,31 @@ def lista_simbolos(simbolo)
       h[campo] = sa.empty? ? nil : sa
     end
   end
-  h
+  return h['simbolos']
 end
 
 
 get '/' do
-  @titulo = 'Configuração inicial: Capa e Folha de Rosto'
-  erb :capa
-end
-
-
-get '/folha' do
-  @titulo = 'Folha de Rosto'
-  ha = params.to_json
-  ya = JSON.load(ha).to_yaml
-  store_yaml("configuracao.yaml", ya)
-  erb :folha
-end
-
-get '/siglas_simbolos' do
-  @titulo = 'Lista de Siglas e Simbolos'
-  ha = params.to_json
-  ya = JSON.load(ha).to_yaml
-  ya = ya[4...-1]  
-  store_yaml("configuracao.yaml", ya)
-  erb :siglas_simbolos
+  @titulo = 'Formulário em HTML para o Limarka. '
+  erb :index
 end
 
 get '/resultados' do
   @titulo = 'Obrigado por utilizar este formulário e o Limarka!'
-  ha = params['siglas']
-  ya = lista_siglas(ha)
-  ya = hash_to_yaml(ya)
-  ya = ya[4...-1]  
-  store_yaml("configuracao.yaml", ya)
-  ha = params['simbolos']
-  ya = lista_simbolos(ha)
-  ya = hash_to_yaml(ya)
-  ya = ya[4...-1]  
-  store_yaml("configuracao.yaml", ya)
+  ha = params.to_json
+  ha = JSON.load(ha)
+  File.open('configuracao.yaml','w') do |h| 
+    h.write ha.to_yaml
+  end  
+  config = YAML.load_file('configuracao.yaml')
+  sig = params['siglas']
+  sig = lista_siglas(sig) 
+  sim = params['simbolos']
+  sim = lista_simbolos(sim)  
+  config['siglas'] = sig
+  config['simbolos'] = sim
+  File.open('configuracao.yaml','w') do |h| 
+    h.write config.to_yaml 
+  end 
   erb :resultados
 end
